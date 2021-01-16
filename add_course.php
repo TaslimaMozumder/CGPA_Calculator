3123caab75645424871663fef75e1b33cb9c31f5
Taslima Mozumder <51834905+TaslimaMozumder@users.noreply.github.com>
2021-01-16 15:44:05 +0600

    Add files via upload

---
+++ add_course.php
@@ -0,0 +1,46 @@
+<?php include_once('assets/php/database.php');
+    if(!isset($_SESSION['login_id'])) {
+        header('location: login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Course</title>
+</head>
+<body>
+    <div class="center">
+        <form action="" method="post">
+            <p>Add New Course</p>
+            <input type="text" name="course_name" placeholder="Course name" required><br><br>
+            <input type="text" name="course_credit" placeholder="Course credit" required><br><br>
+            <input type="text" name="course_semester" placeholder="Course semester" required><br><br>
+            <input type="text" name="course_grade" placeholder="Course grade" required><br><br>
+            <button name="add_course_submit">Add Course</button>
+        </form>
+
+        <?php
+            if(isset($_POST['add_course_submit'])) {
+                $course_name = $_POST['course_name'];
+                $course_credit = $_POST['course_credit'];
+                $course_semester = $_POST['course_semester'];
+                $course_grade = $_POST['course_grade'];
+
+                $user = $_SESSION['login_id'];
+
+                $sql = "INSERT INTO grades (user, course, credit, semester, grade) 
+                VALUES('$user', '$course_name', '$course_credit', '$course_semester', '$course_grade')";
+                    
+                if(mysqli_query($conn, $sql)) {
+                    header('location: index.php');
+                } else {
+                    echo "Add Course Failed";
+                }
+            }
+        ?>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,50 @@
+<?php include_once('assets/php/database.php');
+    if(isset($_SESSION['login_id'])) {
+        header('location: index.php');
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <div class="center">
+        <form action="" method="post">
+            <p>Register Form</p>
+            <input type="text" name="register_id" placeholder="Enter ID" required><br><br>
+            <input type="text" name="register_name" placeholder="Enter name" required><br><br>
+            <input type="password" name="register_password" placeholder="Enter password" required><br><br>
+            <button name="register_submit">Sign Up</button><br><br>
+            <button><a href="login.php">Login</a></button>
+        </form>
+
+        <?php
+            if(isset($_POST['register_submit'])) {
+                $register_id = $_POST['register_id'];
+                $register_name = $_POST['register_name'];
+                $register_password = $_POST['register_password'];
+
+                $search = "SELECT * FROM users WHERE login_id = '$register_id'";
+                $account = mysqli_query($conn, $search);
+        
+                if(mysqli_num_rows($account) > 0) {
+                    echo "Account already exist";
+                } else {
+                    $sql = "INSERT INTO users (login_id, name, password) VALUES('$register_id', '$register_name', '$register_password')";
+                    
+                    if(mysqli_query($conn, $sql)) {
+                        header('location: index.php');
+                    } else {
+                        echo "Registration Failed";
+                    }
+                }
+            }
+        ?>
+    </div>
+</body>
+</html>
